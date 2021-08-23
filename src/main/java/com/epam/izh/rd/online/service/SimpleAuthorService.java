fbdef2c5eb9762703a192acab5b0cfa3c9f8c124
Dmitrii Khaimin <khaimindm@gmail.com>
2021-08-23 23:00:03 +0400

    23.08.2021

---
+++ src/main/java/com/epam/izh/rd/online/service/SimpleAuthorService.java
@@ -0,0 +1,13 @@
+package com.epam.izh.rd.online.service;
+
+public class SimpleAuthorService implements AuthorService{
+
+    boolean save(Author author){}
+
+    Author findByFullName(String name, String lastname){}
+
+    boolean remove(Author author){}
+
+    int count(){}
+
+}
