703f65235ab47a906e1a250bad566c4458611d8c
Dmitrii Khaimin <khaimindm@gmail.com>
2021-08-24 16:03:16 +0400

    24.08.2021

--- src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
@@ -4,14 +4,25 @@ import com.epam.izh.rd.online.entity.Author;
 
 public class SimpleAuthorRepository implements AuthorRepository{
 
-    boolean save(Author author){}
+    int s = 0;
 
-    Author findByFullName(String name, String lastname){}
+    Author[] authors = new Author[s];
+
+    boolean save(Author author){
+        authors[s] = author;
+        s++;
+        authors = new Author[s];
+    }
+
+    Author findByFullName(String name, String lastname){
+        if (name )
+
+    }
 
     boolean remove(Author author){}
 
     int count(){}
 
-    Author[] authors = new Author[0];
+    // Удалить? Author[] authors = new Author[0];
 
 }
