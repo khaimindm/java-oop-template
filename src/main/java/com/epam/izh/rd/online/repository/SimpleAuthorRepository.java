f9141e1200304259a62562eaa5b1d570df41a7ca
Dmitrii Khaimin <khaimindm@gmail.com>
2021-09-13 16:07:49 +0400

    13.09.2021_V2

--- src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
@@ -8,48 +8,32 @@ public class SimpleAuthorRepository implements AuthorRepository{
     int p = 0;
     int t = 0;
 
-    //Author author = new Author();
-
     private Author[] authors = new Author[sAA];
     private Author[] authors1 = new Author[sAA];
+
 
     //SimpleAuthorRepository fBFN = new SimpleAuthorRepository();
 
 
     public boolean save(Author author){
 
-        String h;
-        String h2;
-        //Author g;
-
         boolean resultS = false;
+
         if (authors[p] == null) {
             authors[p] = new Author();
         }
 
-        /*h = author.getName();
-        g = findByFullName(author.getName(), author.getLastName());*/
-
         if (findByFullName(author.getName(), author.getLastName()) == null) {  //fBFN.
-            //authors[p] = new Author();
             authors[p] = author;
-
-            /*g = authors[p];
-            h = g.getName();
-            h2 = h;*/
-
             sAA++;
             p++;
             authors1 = new Author[authors.length];
             System.arraycopy(authors, 0, authors1, 0, authors.length);
             authors = new Author[sAA];
             System.arraycopy(authors1, 0, authors, 0, authors1.length);
+
             //authors[sAA -1] = new Author();
-            System.out.println(authors[p-1].getName() + authors[p-1].getLastName());
-            h = authors[p-1].getName();
-            h2 = authors[p-1].getLastName();
-            authors[p-1].getName();
-            authors[p-1].getLastName();
+
             resultS = true;
 
         } else {
@@ -65,10 +49,6 @@ public class SimpleAuthorRepository implements AuthorRepository{
         Author resultF = null;
         int c = 0;
 
-        /*String test;
-        String test2;
-        test = author.getName();*/
-
         for (int i = 0; i < authors.length; i++) {
             if (authors[i] == null) {
                 authors[i] = new Author();
@@ -83,6 +63,7 @@ public class SimpleAuthorRepository implements AuthorRepository{
             }
             c++;
         }
+
         return resultF;
 
     }
--- src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
@@ -5,31 +5,76 @@ import com.epam.izh.rd.online.entity.SchoolBook;
 
 public class SimpleSchoolBookRepository implements BookRepository<SchoolBook> {
 
-    int sASB = 0;
+    int sASB = 1;
+    int pB = 0;
 
     private SchoolBook[] schoolBooks = new SchoolBook[sASB];
+    private SchoolBook[] schoolBooks1 = new SchoolBook[sASB];
 
     public boolean save(SchoolBook book){
 
-        schoolBooks[sASB] = book;
-        sASB++;
-        schoolBooks = new SchoolBook[sASB];
-        return true;
+        boolean resultss = false;
+
+        if (sASB != 1) {
+            schoolBooks1 = new SchoolBook[schoolBooks.length];
+            System.arraycopy(schoolBooks, 0, schoolBooks1, 0, schoolBooks.length);
+            sASB++;
+            schoolBooks = new SchoolBook[sASB];
+            System.arraycopy(schoolBooks1, 0, schoolBooks, 0, schoolBooks1.length);
+            schoolBooks[sASB - 1] = new SchoolBook();
+            schoolBooks[sASB - 1] = book;
+            resultss = true;
+        } else {
+            schoolBooks[0] = new SchoolBook();
+            schoolBooks[0] = book;
+            sASB++;
+            resultss = true;
+        }
+
+        return resultss;
 
     }
 
     public SchoolBook[] findByName(String name) {
 
-        int sASBf = 0;
-        SchoolBook[] schoolBooks = new SchoolBook[sASBf];
+        int sASBf = 1;
+        int pSB2f = 0;
+
+        SchoolBook[] schoolBooks = new  SchoolBook[sASBf];
+        SchoolBook[] schoolBooks2 = new SchoolBook[sASBf];
+
+
+
+
         for (int i=0; i< this.schoolBooks.length; i++) {
             if (name == this.schoolBooks[i].getName()) {
-                schoolBooks = new SchoolBook[sASBf++];
-                schoolBooks[i] = this.schoolBooks[i];
-                return schoolBooks;
+                sASBf++;
             }
         }
+
+        schoolBooks2 = new SchoolBook[sASBf];
+
+        for (int i = 0; i < this.schoolBooks.length; i++) {
+            if (name == this.schoolBooks[i].getName()) {
+                schoolBooks2[pSB2f] = this.schoolBooks[i];
+                pSB2f++;
+            }
+        }
+
+        schoolBooks = new SchoolBook[sASBf];
+
+        schoolBooks = schoolBooks2;
+
         return schoolBooks;
+
+
+        /*schoolBooks2[pSB2f] = this.schoolBooks[i];
+        System.arraycopy();
+
+        schoolBooks = new SchoolBook[sASBf++];
+        schoolBooks[i] = this.schoolBooks[i];
+        return schoolBooks;
+        return schoolBooks;*/
         
     }
 
